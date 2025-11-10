cee9e65214e276083fc483d107b04e20992cf657
maruta <maruta-r.83@mb.meidensha.co.jp>
2025-11-10 16:41:41 +0900

    ?? projectUI can't do

--- project_management_app/src/components/Sidebar.tsx
+++ project_management_app/src/components/Sidebar.tsx
@@ -29,7 +29,6 @@ export default function Sidebar() {
                         <path fillRule="evenodd" d="M6.912 3a3 3 0 00-2.868 2.118l-2.411 7.838a3 3 0 00-.133.882V18a3 3 0 003 3h15a3 3 0 003-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0017.088 3H6.912zm13.823 9.75l-2.213-7.191A1.5 1.5 0 0017.088 4.5H6.912a1.5 1.5 0 00-1.434 1.059L3.265 12.75H6.11a3 3 0 012.684 1.658l.256.513a1.5 1.5 0 001.342.829h3.218a1.5 1.5 0 001.342-.83l.256-.512a3 3 0 012.684-1.658h2.844z" clipRule="evenodd"></path>
                         </svg>
                     </div>
-                    <Link to="/project">Project</Link>
                     </div>
                     <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -43,6 +42,8 @@ export default function Sidebar() {
                     <a href="#" aria-current="true" className="block w-full px-4 py-2 text-white bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer dark:bg-gray-800 dark:border-gray-600">
                         projectA
                     </a>
+                    <Link to="/project">Project</Link>
+
                     <a href="#" className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
                         projectB
                     </a>
--- project_management_app/src/pages/Project.tsx
+++ project_management_app/src/pages/Project.tsx
@@ -1,10 +1,69 @@
 import "./Project.css"
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
 
 export default function Project() {
+ 
+    const data = [
+    {
+        label: "HTML",
+        value: "html",
+        desc: `It really matters and then like it really doesn't matter.
+        What matters is the people who are sparked by it. And the people 
+        who are like offended by it, it doesn't matter.`,
+    },
+    {
+        label: "React",
+        value: "react",
+        desc: `Because it's about motivating the doers. Because I'm here
+        to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+    {
+        label: "Vue",
+        value: "vue",
+        desc: `We're not always in the position that we want to be at.
+        We're constantly growing. We're constantly making mistakes. We're
+        constantly trying to express ourselves and actualize our dreams.`,
+    },
+    {
+        label: "Angular",
+        value: "angular",
+        desc: `Because it's about motivating the doers. Because I'm here
+        to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+    {
+        label: "Svelte",
+        value: "svelte",
+        desc: `We're not always in the position that we want to be at.
+        We're constantly growing. We're constantly making mistakes. We're
+        constantly trying to express ourselves and actualize our dreams.`,
+    },
+    ];
 
-    return (
-        <h2>プロジェクト画面</h2>
-    );
+  return (
+    <div>
+    <Tabs value="html" className="sss">
+      <TabsHeader placeholder={undefined} onResize={undefined} onResizeCapture={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+        {data.map(({ label, value }) => (
+          <Tab key={value} value={value} placeholder={undefined} onResize={undefined} onResizeCapture={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+            {label}
+          </Tab>
+        ))}
 
-
-}
+      </TabsHeader>
+      <TabsBody placeholder={undefined} onResize={undefined} onResizeCapture={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+        {data.map(({ value, desc }) => (
+          <TabPanel key={value} value={value}>
+            {desc}
+          </TabPanel>
+        ))}
+      </TabsBody>
+    </Tabs>
+    </div>
+  );
+}   
